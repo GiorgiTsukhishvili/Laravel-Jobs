8fa40dc5443173a6107bed90e2097d0f25b0f330
giorgi <giorgitsukhishvili@redberry.ge>
2022-11-02 21:47:13 +0400

    Adding navbar hero and footer

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -1,17 +1,27 @@
 <!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<html lang="en">
     <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta charset="UTF-8" />
+        <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <link rel="icon" href="images/favicon.ico" />
+        <link
+            rel="stylesheet"
+            href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css"
+            integrity="sha512-KfkfwYDsLkIlwQp6LFnl8zNdLGxu9YAA1QvwINks4PhcElQSvqcyVLLD9aMhXd13uQjoXtEKNosOWaZqXgel0g=="
+            crossorigin="anonymous"
+            referrerpolicy="no-referrer"
+        />
+        @vite('resources/css/app.css')
 
-        <title>Laravel</title>
-
-        <link href="https://fonts.bunny.net/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
-
-@vite('resources/css/app.css')
-   
+        <title>Find Laravel Jobs & Projects</title>
     </head>
- <body class=''>
+    <body class="mb-48">
+        <x-navbar/>
+        <x-hero/>
+<main>
 {{$slot}}
-</body>
-    </html>
+</main>
+  <x-footer/>
+    </body>
+</html>
