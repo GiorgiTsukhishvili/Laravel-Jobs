55eda8d46a9979cce9402a726fd9661ca69285de
giorgi <gcuxishvili@gmail.com>
2022-11-05 18:51:51 +0400

    creating register route and markup of form

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\ListingController;
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -27,3 +28,5 @@ Route::get('/listings/{list:title}', [ListingController::class, 'oneList']);
 Route::get('/listings/{listing}/edit', [ListingController::class, 'edit']);
 
 Route::delete('/listings/{listing}/delete', [ListingController::class, 'delete']);
+
+Route::get('/register', [UserController::class, 'create']);
